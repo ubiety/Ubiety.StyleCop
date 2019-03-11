171832f0d731f7a5e8db40cf2290f427d5e9f879
Dieter Lunn <coder2000@gmail.com>
2019-03-10 22:38:04 -0500

    build: update build script to publish library

--- build/Build.cs
+++ build/Build.cs
@@ -2,6 +2,7 @@ using Nuke.Common;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.FileSystemTasks;
@@ -18,10 +19,13 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.Publish);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter] readonly string ApiKey;
+    [Parameter] readonly string Source = "https://api.nuget.org/v3/index.json";
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
@@ -46,14 +50,29 @@ class Build : NukeBuild
                 .SetProjectFile(Solution));
         });
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
+    Target Pack => _ => _
         .Executes(() =>
         {
-            DotNetBuild(s => s
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .EnableNoRestore());
+            DotNetPack(s => s
+                .SetOutputDirectory(ArtifactsDirectory)
+                .EnableNoBuild()
+                .SetProject(Solution)
+                .SetConfiguration(Configuration));
+        });
+
+    Target Publish => _ => _
+        .DependsOn(Clean, Restore, Pack)
+        .Requires(() => ApiKey)
+        .Executes(() =>
+        {
+            DotNetNuGetPush(s => s
+                .SetApiKey(ApiKey)
+                .SetSource(Source)
+                .CombineWith(
+                    ArtifactsDirectory.GlobFiles("*.nupkg").NotEmpty(), (cs, v) => 
+                        cs.SetTargetPath(v)),
+                5,
+                true);
         });
 
 }
